bf79e4229a38eeb60c570c9e2ef24f694676d6aa
清遠翔太 <shota@shinenshoutanoMacBook-Air.local>
2020-02-28 11:19:46 +0900

    指摘対応

--- user.js
+++ user.js
@@ -1,12 +1,13 @@
 
+//ここからユーザー登録
+function info(){
+const name = document.getElementById('name').value;
+const bio = document.getElementById('bio').value;
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+const password_confirmation= document.getElementById('password_confirmation').value;
 
-cconst name = document.getElementById('name');
-const bio = document.getElementById('bio');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password_confirmation= document.getElementById('password_confirmation');
 
-//ここからユーザー登録//
 var url = 'https://teachapi.herokuapp.com/sign_up';
 const sign_up_user_params = [name,bio,email,password,password_confirmation];
 
@@ -20,7 +21,7 @@ fetch(url, {
 .then(response => console.log('Success:', JSON.stringify(response)))
 .catch(error => console.error('Error:', error));
 
-
+};
 
 
 //ここからログイン
